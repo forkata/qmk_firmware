b34fafadf99d35db03ca822d48f56ec2eba135b1
Chris Todorov <chris@freerunningtechnologies.com>
2016-09-07 01:57:21 -0700

    It works!
    First keymap for the FortyEight! Let's see how long this will last.

--- keyboards/fortyeight/keymaps/default/keymap.c
+++ keyboards/fortyeight/keymaps/default/keymap.c
@@ -1,44 +1,62 @@
 #include "fortyeight.h"
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-[0] = KEYMAP( /* Base */
-  KC_A,  KC_1,  KC_H, \
-    KC_TAB,  KC_SPC   \
+  /* 0: Base
+   * ,-----------------------------------------------------------.
+   * |Esc  |  Q|  w|  E|  R|  T|  Y|  U|  I|  O|  P|  [|  ]| Bspc|
+   * |-----------------------------------------------------------|
+   * |Tab  |  A|  S|  D|  F|  G|  H|  J|  K|  L|  ;|  '|    Enter|
+   * |-----------------------------------------------------------|
+   * |Shift    |  Z|  X|  C|  V|  B|  N|  M|  ,|  .|   /|Shft|Fn4|
+   * |-----------------------------------------------------------'
+   * |Ctrl | Gui| Alt|            Space          |Fn0|Fn1|Fn2|Fn3|
+   * `-----------------------------------------------------------'
+   */
+[0] = KEYMAP(
+  ESC,  Q,  W,  E,  R,  T,  Y,  U,  I,  O,  P, LBRC,  RBRC, BSPC, \
+  TAB,  A,  S,  D,  F,  G,  H,  J,  K,  L,  SCLN,  QUOT,  ENTER, \
+  LSFT,  Z,  X, C,  V,  B,  N,  M, COMMA,  DOT,  SLASH,  RSFT,  FN4, \
+  LCTL,  LGUI,  LALT, SPC,  FN0, FN1, FN2, FN3 \
 ),
+
+  /* 1: Fn0
+   * ,-----------------------------------------------------------.
+   * |~    |  !|  @|  #|  $|  %|  ^|  &|  *|  (|  )|  _|  +| Del |
+   * |-----------------------------------------------------------|
+   * |`    |  1|  2|  3|  4|  5|  6|  7|  8|  9|  0|  ||         |
+   * |-----------------------------------------------------------|
+   * |         |   |   |   |   |   |   |   |   |   |    |    |   |
+   * |-----------------------------------------------------------'
+   * |    |    |     |                           |   |   |   |   |
+   * `-----------------------------------------------------------'
+   */
+[1] = KEYMAP(
+  TILD, EXLM,  AT,  HASH,  DLR,  PERC,  CIRC,  AMPR,  ASTR,  LPRN,  RPRN,  UNDS, PLUS, DEL, \
+  GRV,  1,  2,  3,  4,  5,  6,  7,  8,  9,  0,  PIPE,  TRNS, \
+  TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, \
+  TRNS,  TRNS,  TRNS, TRNS, TRNS, TRNS, TRNS, TRNS \
+),
+
+  /* 2: Fn1
+   * ,-----------------------------------------------------------.
+   * |     |   | Up|   |   |   |   |   |   |   |   |   |   |     |
+   * |-----------------------------------------------------------|
+   * |     |Lef|Dow|Rig|   |   |   |   |   |   |   |   |         |
+   * |-----------------------------------------------------------|
+   * |         |   |   |   |   |   |   |   |   |   |    |    |   |
+   * |-----------------------------------------------------------'
+   * |    |    |     |                           |   |   |   |   |
+   * `-----------------------------------------------------------'
+   */
+[2] = KEYMAP(
+  TRNS, TRNS, UP,   TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, \
+  TRNS, LEFT, DOWN, RGHT, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, \
+  TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, \
+  TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS \
+)
 };
 
 const uint16_t PROGMEM fn_actions[] = {
-
+  ACTION_LAYER_MOMENTARY(1),                  // FN0
+  ACTION_LAYER_MOMENTARY(2)                  // FN1
 };
-
-const macro_t *action_get_macro(keyrecord_t *record, uint8_t id, uint8_t opt)
-{
-  // MACRODOWN only works in this function
-      switch(id) {
-        case 0:
-          if (record->event.pressed) {
-            register_code(KC_RSFT);
-          } else {
-            unregister_code(KC_RSFT);
-          }
-        break;
-      }
-    return MACRO_NONE;
-};
-
-
-void matrix_init_user(void) {
-
-}
-
-void matrix_scan_user(void) {
-
-}
-
-bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-  return true;
-}
-
-void led_set_user(uint8_t usb_led) {
-
-}
